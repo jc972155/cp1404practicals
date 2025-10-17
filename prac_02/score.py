fb08d62290d0b085010dfc24c0f702d6aac48f59
jc972155 <HARRY.MCRAE@MY.JCU.EDU.AU>
2025-10-17 13:22:43 +1000

    Add completed score file

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,23 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    print(evaluate_score(score))
+    random_score = random.randint(0,100)
+    print("Random score is", random_score)
+    print(evaluate_score(random_score))
+
+def evaluate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
