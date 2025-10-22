b5dc95d8c7d25471baf6750d4a062b79a0039150
jc972155 <HARRY.MCRAE@MY.JCU.EDU.AU>
2025-10-22 11:51:16 +1000

    Add completed hex_colour prac_05

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,15 +4,16 @@ save colours to a dictionary
 input name and find hex code
 """
 
-COLOUR_TO_HEX = {"amber": "#ffbf00", "barn red": "#7c0a02", "Camel": "#c19a6b", "Denim": "#1560bd", "Eminence": "#6c3082", "Fulvous": "#e48400", "Ginger": "#b06500", "Green Lizard": "#a7f432", "Harlequin": "#3fff00", "Iris": "#5a4fcf", "Jasmine": "#f8de7e"}
+COLOUR_TO_HEX = {"AMBER": "#ffbf00", "BARN RED": "#7c0a02", "CAMEL": "#c19a6b", "DENIM": "#1560bd", "EMINENCE": "#6c3082", "FULVOUS": "#e48400", "GINGER": "#b06500", "GREEN LIZARD": "#a7f432", "HARLEQUIN": "#3fff00", "IRIS": "#5a4fcf", "JASMINE": "#f8de7e"}
 
 for name in COLOUR_TO_HEX.keys():
     print(name)
 
-colour_name = input("Enter short state: ").upper()
+colour_name = input("Enter a colour: ").upper()
 while colour_name != "":
     try:
-        print(colour_name, "is", COLOUR_TO_HEX[state_code])
+        print(colour_name, "is", COLOUR_TO_HEX[colour_name])
     except KeyError:
-        print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+        print("Enter a colour on the list")
+    colour_name = input("Enter a colour: ").upper()
+print("Program Finished")
