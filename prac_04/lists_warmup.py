9110828eb7113c2059fdd1b326bfba6376008a25
jc972155 <HARRY.MCRAE@MY.JCU.EDU.AU>
2025-11-06 16:17:31 +1000

    add completed lists_warmup to prac_04

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,22 @@
+"""
+CP1404 prac_04
+lists warm up
+"""
+
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0] # 3
+numbers[-1] # 2
+numbers[3] # 1
+numbers[:-1] # 3 1 4 1 5 9
+numbers[3:4] # 1 5 (wrong, only 1)
+5 in numbers # True
+7 in numbers # False
+"3" in numbers # False
+numbers + [6, 5, 3] # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "10"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
