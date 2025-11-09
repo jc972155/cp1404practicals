2e253255489b35ab019317643f676e895e9e82fa
jc972155 <HARRY.MCRAE@MY.JCU.EDU.AU>
2025-11-09 18:46:19 +1000

    Complete part 2 of More Guitars exercise.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -11,12 +11,13 @@ def main():
     """Read file of guitars, save as objects, display."""
     guitars = []
     in_file = open('guitars.csv', 'r')
-    in_file.readline()
     for line in in_file:
         parts = line.strip().split(',')
         guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
         guitars.append(guitar)
     in_file.close()
+
+    add_guitar(guitars)
     guitars.sort()
     print_guitars(guitars)
 
@@ -25,6 +26,20 @@ def print_guitars(guitars):
     """Print the guitars"""
     for guitar in guitars:
         print(guitar)
+
+
+def add_guitar(guitars):
+    get_guitar(guitars)
+    with open("guitars.csv", "w") as out_file:
+        for guitar in guitars:
+            out_file.write(f"{guitar.name},{str(guitar.year)},{str(guitar.cost)}\n")
+
+
+def get_guitar(guitars):
+    name = input("Enter new guitar name: ")
+    year = int(input("Year of manufacture: "))
+    cost = float(input("Cost: "))
+    guitars.append(Guitar(name, year, cost))
 
 
 main()
