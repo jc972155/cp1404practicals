02213416e8be472aab5f1a7626b6e16155b2b6aa
jc972155 <HARRY.MCRAE@MY.JCU.EDU.AU>
2025-11-20 20:22:34 +1000

    add completed taxi_test to prac_09

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,24 @@
+"""
+CP1404 Practical
+Create an object in the Taxi class
+"""
+
+from taxi import Taxi
+
+# Initialise taxi object
+my_taxi = Taxi("Prius 1", 100, 1.23)
+
+# Drive 40km
+my_taxi.drive(40)
+
+# Print taxi details and fare
+print(my_taxi)
+print(my_taxi.get_fare())
+
+# Restart and drive 100km
+my_taxi.start_fare()
+my_taxi.drive(100)
+
+# Print details and fare
+print(my_taxi)
+print(my_taxi.get_fare())
