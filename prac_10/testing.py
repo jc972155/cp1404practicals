7e2dc34de1a3df4a2c4b2f3461830aadd6155477
jc972155 <HARRY.MCRAE@MY.JCU.EDU.AU>
2025-11-23 15:38:10 +1000

    add assert statements to test Car

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -29,7 +29,7 @@ def run_tests():
     """Run the tests on the functions."""
     # assert test with no message - used to see if the function works properly
     assert repeat_string("Python", 1) == "Python"
-    # the test below should fail
+    # the test below now passes
     assert repeat_string("hi", 2) == "hi hi"
 
     # 1. fix the repeat_string function above so that it passes the failing test
@@ -41,12 +41,16 @@ def run_tests():
     car = Car()
     assert car._odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
+    # 2. Write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
-    car = Car(fuel=10)
 
+    # Test default
+    assert car.fuel == 0, "Default does set correctly"
+    # Test passed value
+    car = Car(fuel=10)
+    assert car.fuel == 10, "Passed value does not set correctly"
 
 run_tests()
 
